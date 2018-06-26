88a4abeb48fc6728fbd8e839f7a250417955450b
IN\Dinesh.R <dineshapp22@gmail.com>
2018-06-26 15:44:27 +0530

    Excel file for username and password

---
+++ sample/src/testscripts/Bean.java
@@ -0,0 +1,30 @@
+package testscripts;
+
+public class Bean {
+	String userName;
+	String password;
+	String result;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getResult() {
+		return result;
+	}
+	public void setResult(String result) {
+		this.result = result;
+	}
+	@Override
+	public String toString() {
+		return "Bean [userName=" + userName + ", password=" + password + ", result=" + result + "]";
+	}
+	
+}
--- sample/src/testscripts/HelloWorld.java
+++ sample/src/testscripts/HelloWorld.java
@@ -1,58 +1,194 @@
 package testscripts;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.net.URL;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
-
 
 public class HelloWorld {
 
-	public static void main(String[] args) throws InterruptedException {
-		
-		System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
-		
-		WebDriver driver = new ChromeDriver();
-		
-		String baseurl="https://10.2.74.204/#/login";
-		/*String expectedTitle="Welcome: Mercury Tours";
-		String actualTitle = "";*/
-		
-		driver.get(baseurl);
-		//actualTitle=driver.getTitle();
-		
-       
-		driver.findElement(By.id("inputUsername")).sendKeys("admin");
-		driver.findElement(By.id("inputPassword3")).sendKeys("passw0rd");
-		driver.findElement(By.xpath("//button[contains(text(),'Sign in')]")).submit();
-		//driver.findElement(By.name("submit")).click();
-		
-		//System.out.println("Home page reached");Home
-		//driver.findElement(By.tex("Home")).click();
-		  //driver.findElements By.xpath("//*//[contains(text(),'Home')]"));
-		
-    	System.out.println("Test Failed");
-    	driver.close();
-    }
-		//String newurl="https://10.2.74.204/#/pages/home";
-		/*String nUrl= driver.getCurrentUrl();
-		System.out.println(nUrl);
-		
-		if (nUrl.equalsIgnoreCase("https://10.2.74.204/#/pages/home"))
-				{
-			System.out.println("Test Passed!");
-    } else {
-        System.out.println("Test Failed");
-    }
-		//boolean sample = driver.getPageSource().contains("Home");
-		//System.out.println(sample);
-		//driver.wait(5000);
-		
-		*/
-		
+	private static final String FILE_NAME = "C:/Users/Dinesh.IN/Downloads/form1.xlsx";
+	static String username;
+	static String password;
+	public List<Bean> getReadXLData(Iterator<Row> iterator ) {
+		List <Bean> li=new ArrayList<Bean>();
+
+		try {
+
+			while (iterator.hasNext()) {
+
+				Row currentRow = iterator.next();
+				Iterator<Cell> cellIterator = currentRow.iterator();
+				Bean bean= new Bean();
+				List<String> list=new ArrayList<>();
+				while (cellIterator.hasNext()) {
+
+					Cell currentCell = cellIterator.next();
+
+					if (currentCell.getCellTypeEnum() == CellType.STRING) {
+						list.add(currentCell.getStringCellValue());
+						System.out.print(currentCell.getStringCellValue() + "==");
+					} else if (currentCell.getCellTypeEnum() == CellType.NUMERIC) {
+						// System.out.print("Numeric values......");
+					}
+
+				}
+				bean.setUserName(list.size()>0?list.get(0):"");
+				bean.setPassword(list.size()>1?list.get(1):"");
+				bean.setResult(list.size()>2?list.get(2):"");
+				li.add(bean);
+				System.out.println();
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return	li;
 	}
 
 
+	public static void main(String[] args) throws InterruptedException {
+
+		HelloWorld hl=new HelloWorld();
+		List<Bean> li=new ArrayList<Bean>();
+		XSSFSheet datatypeSheet = null;
+
+		FileInputStream excelFile;
+		try {
+			excelFile = new FileInputStream(new File(FILE_NAME));
+			XSSFWorkbook workbook = new XSSFWorkbook(excelFile);
+			datatypeSheet = workbook.getSheetAt(0);
+			Iterator<Row> iterator = datatypeSheet.iterator();
+			li=hl.getReadXLData(iterator);
+
+
+
+
+
+			System.out.println("list---:"+li);
+			username=li.get(0).getUserName();
+			password=li.get(0).getPassword();
+			// hl.window(username,password);
+
+
+			System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
+
+			WebDriver driver = new ChromeDriver();
+
+			String baseurl="http://demo.guru99.com/test/newtours/";
+			/*String expectedTitle="Welcome: Mercury Tours";
+		String actualTitle = "";*/
+
+			driver.get(baseurl);
+			//actualTitle=driver.getTitle();
+			int rownum=0;
+			for(Bean bean:li) {
+				System.out.println("UserName .."+bean.getUserName());
+				System.out.println("Password .."+bean.getPassword());
+				driver.findElement(By.name("userName")).sendKeys(bean.getUserName());
+				driver.findElement(By.name("password")).sendKeys(bean.getPassword());
+				driver.findElement(By.name("submit")).click();
+				boolean sample = driver.getPageSource().contains("Login Successfully");
+				if (sample==true){
+					System.out.println("Test Passed!");
+					bean.setResult("Pass");
+					driver.get(baseurl);
+
+				} else {
+					System.out.println("Test Failed");
+					bean.setResult("Fail");
+
+				}
+				Row row = datatypeSheet.getRow(rownum);
+				Cell cell=row.createCell(2);
+				cell.setCellValue(bean.getResult());
+				rownum++;
+			}
+			excelFile.close();
+			FileOutputStream outputStream = new FileOutputStream(new File(FILE_NAME));
+
+			//write data in the excel file
+
+			workbook.write(outputStream);
+
+			//close output stream
+
+			outputStream.close();
+			driver.close();
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+
+
+	public void window(String username,String passowrd) {
+		System.out.print("username :"+username +"  Password :"+password);	
+
+	}
+
+
+	/*public List<Bean> getWriteXLData() {
+		List <Bean> li=new ArrayList<Bean>();
+
+		try {
+
+			FileInputStream excelFile = new FileInputStream(new File(FILE_NAME));
+			XSSFWorkbook workbook = new XSSFWorkbook(excelFile);
+			XSSFSheet datatypeSheet = workbook.getSheetAt(0);
+			Iterator<Row> iterator = datatypeSheet.iterator();
+
+
+			while (iterator.hasNext()) {
+
+				Row currentRow = iterator.next();
+				Iterator<Cell> cellIterator = currentRow.iterator();
+				Bean bean= new Bean();
+				List<String> list=new ArrayList<>();
+				while (cellIterator.hasNext()) {
+
+					Cell currentCell = cellIterator.next();
+
+					System.out.println("current cell:"+currentCell);
+
+					if (currentCell.getCellTypeEnum() == CellType.STRING) {
+
+						list.add(currentCell.setCellValue());
+						System.out.print(currentCell.SetCellValue() + "==");
+					} else if (currentCell.getCellTypeEnum() == CellType.NUMERIC) {
+						// System.out.print("Numeric values......");
+					}
+
+				}
+				bean.setResult(list.size()>2?list.get(2):"");
+				li.add(bean);
+				System.out.println();
+
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return	li;
+	}*/
+}
